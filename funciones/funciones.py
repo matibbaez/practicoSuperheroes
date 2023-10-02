8d60f91cb7cd92008df02f639cbf685fc03abee0
Matias Baez <matiascaneda3@gmail.com>
2023-10-02 18:57:37 -0300

    funciones segunda parte

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,5 +1,6 @@
 from superheroes.personajes import *
 
+# ---> Primera parte
 def mostrar_superheroes(lista_personajes):
     for personaje in lista_personajes:
         print(f"Nombre: {personaje['nombre']}")    
@@ -65,10 +66,9 @@ def menos_pesado (lista_personajes):
     
     print(f"El personaje menos pesado es: {nombre_menos_peso}, con un peso de: {peso_min} kilos.")
     
-
 def mostrar_menu ():
     menu = (
-        "\n   *** Menu de opciones: ***   \n"
+        "\n   *** Primera parte: ***   \n"
         "1. Nombres de los superheroes\n"
         "2. Altura de los superheroes\n"
         "3. Superheroe mas alto\n"
@@ -76,5 +76,148 @@ def mostrar_menu ():
         "5. Promedio de altura de los superheroes\n"
         "6. Superheroe mas pesado\n"
         "7. Superheroe menos pesado\n"
+    )
+    print(menu)
+    
+# ---> Segunda parte
+
+def genero_m (lista_personajes):
+    for personaje in lista_personajes:
+        if personaje['genero'] == "M":
+            print(f"Nombre: {personaje['nombre']}")
+            
+
+def genero_f (lista_personajes):
+    for personaje in lista_personajes:
+        if personaje['genero'] == "F":
+            print(f"Nombre: {personaje['nombre']}")
+            
+def mas_alto_m (lista_personajes):
+    altura_max_m = None
+    nombre_mas_alto_m = ""
+        
+    for personaje in lista_personajes:
+        if personaje['genero'] == "M":
+            altura_actual = float(personaje['altura'])
+            if altura_max_m is None or altura_actual > altura_max_m:
+                altura_max_m = altura_actual
+                nombre_mas_alto_m = personaje['nombre']
+    
+    if nombre_mas_alto_m:
+        print(f"El personaje masculino más alto es: {nombre_mas_alto_m}, con una altura de: {altura_max_m} cm.")
+
+def mas_alto_f (lista_personajes):
+    altura_max_f = None
+    nombre_mas_alto_f = ""
+        
+    for personaje in lista_personajes:
+        if personaje['genero'] == "F":
+            altura_actual = float(personaje['altura'])
+            if altura_max_f is None or altura_actual > altura_max_f:
+                altura_max_f = altura_actual
+                nombre_mas_alto_f = personaje['nombre']
+    
+    if nombre_mas_alto_f:
+        print(f"El personaje femenimo más alto es: {nombre_mas_alto_f}, con una altura de: {altura_max_f} cm.")
+        
+def mas_bajo_m (lista_personajes):
+    altura_min_m = None
+    nombre_mas_bajo_m = ""
+        
+    for personaje in lista_personajes:
+        if personaje['genero'] == "M":
+            altura_actual = float(personaje['altura'])
+            if altura_min_m is None or altura_actual < altura_min_m:
+                altura_min_m = altura_actual
+                nombre_mas_bajo_m = personaje['nombre']
+    
+    if nombre_mas_bajo_m:
+        print(f"El personaje masculino más bajo es: {nombre_mas_bajo_m}, con una altura de: {altura_min_m} cm.")
+
+def mas_bajo_f (lista_personajes):
+    altura_min_f = None
+    nombre_mas_bajo_f = ""
+        
+    for personaje in lista_personajes:
+        if personaje['genero'] == "F":
+            altura_actual = float(personaje['altura'])
+            if altura_min_f is None or altura_actual < altura_min_f:
+                altura_min_f = altura_actual
+                nombre_mas_bajo_f = personaje['nombre']
+    
+    if nombre_mas_bajo_f:
+        print(f"El personaje femenino más bajo es: {nombre_mas_bajo_f}, con una altura de: {altura_min_f} cm.")
+
+def promedio_m (lista_personajes):
+    acumulador_altura_m = 0
+    
+    for personaje in lista_personajes:
+        if personaje['genero'] == "M":
+            acumulador_altura_m = acumulador_altura_m + float(personaje['altura'])
+    promedio = acumulador_altura_m // len(lista_personajes)
+    
+    print(f"El promedio de altura masculino es: {promedio}cm")        
+
+def promedio_f (lista_personajes):
+    acumulador_altura_f = 0
+    
+    for personaje in lista_personajes:
+        if personaje['genero'] == "F":
+            acumulador_altura_f = acumulador_altura_f + float(personaje['altura'])
+    promedio = acumulador_altura_f // len(lista_personajes)
+    
+    print(f"El promedio de altura femenino es: {promedio}cm")        
+
+def contador_color_ojos(lista_personajes):
+    contador_color_ojos = {}
+
+    for personaje in lista_personajes:
+        color_ojos = personaje['color_ojos']
+        contador_color_ojos[color_ojos] = contador_color_ojos.get(color_ojos, 0) + 1
+
+    print("Número de superhéroes por color de ojos:")
+    for color in contador_color_ojos:
+        count = contador_color_ojos[color]
+        print(f"{color}: {count}")
+
+def contador_color_pelo(lista_personajes):
+    contador_color_pelo = {}
+
+    for personaje in lista_personajes:
+        color_pelo = personaje['color_pelo']
+        if color_pelo: 
+            contador_color_pelo[color_pelo] = contador_color_pelo.get(color_pelo, 0) + 1
+
+    print("\nNúmero de superhéroes por color de pelo:")
+    for color in contador_color_pelo:
+        count = contador_color_pelo[color]
+        print(f"{color}: {count}")
+
+def contador_inteligencia(lista_personajes):
+    contador_inteligencia = {}
+
+    for personaje in lista_personajes:
+        inteligencia = personaje.get('inteligencia', 'No Tiene')
+        if inteligencia: 
+            contador_inteligencia[inteligencia] = contador_inteligencia.get(inteligencia, 0) + 1
+
+    print("\nNúmero de superhéroes por tipo de inteligencia:")
+    for inteligencia, count in contador_inteligencia.items():
+        print(f"{inteligencia}: {count}")
+        
+def mostrar_segundo_menu():
+    menu = (
+        "\n   *** Segunda parte: ***   \n"
+        "1. Nombres de los superheroes masculinos\n"
+        "2. Nombres de los superheroes femeninos\n"
+        "3. Mas alto del genero masculino\n"
+        "4. Mas alto del genero femenino\n"
+        "5. Mas bajo del genero masculino\n"
+        "6. Mas bajo del genero femenino\n"
+        "7. Altura promedio genero masculino\n"
+        "8. Altura promedio genero femenino\n"
+        "9. Cantidad de colores de ojos\n"
+        "10. Cantidad de colores de pelo\n"
+        "11. Tipos y cantidad de inteligencia\n"
     )
     print(menu)
--- main.py
+++ main.py
@@ -1,27 +1,62 @@
 from superheroes.personajes import lista_personajes
-from funciones.funciones import mostrar_menu, mostrar_superheroes, altura_superheroes, mas_alto, mas_bajo, promedio_altura, mas_pesado, menos_pesado
+from funciones.funciones import *
 
-while True:
-    mostrar_menu()
-    opcion = input("Elija una opción (1-7, o 8 para salir): ")
+menu = input("Elija un menu (1 o 2): ")
+if menu == "1":
+    while True:
+        mostrar_menu()
+        opcion = input("Elija una opción (1-7, o 8 para salir): ")
 
-    if opcion == '1':
-        mostrar_superheroes(lista_personajes)
-    elif opcion == '2':
-        altura_superheroes(lista_personajes)
-    elif opcion == '3':
-        mas_alto(lista_personajes)
-    elif opcion == '4':
-        mas_bajo(lista_personajes)
-    elif opcion == '5':
-        promedio_altura(lista_personajes)
-    elif opcion == '6':
-        mas_pesado(lista_personajes)
-    elif opcion == '7':
-        menos_pesado(lista_personajes)
-    elif opcion == '8':
-        print("Saliendo del programa. ¡Hasta luego!")
-        break
-    else:
-        print("Opción no válida. Elija una opción del 1 al 8.")
-        input("Presione Enter para continuar...")
+        if opcion == '1':
+            mostrar_superheroes(lista_personajes)
+        elif opcion == '2':
+            altura_superheroes(lista_personajes)
+        elif opcion == '3':
+            mas_alto(lista_personajes)
+        elif opcion == '4':
+            mas_bajo(lista_personajes)
+        elif opcion == '5':
+            promedio_altura(lista_personajes)
+        elif opcion == '6':
+            mas_pesado(lista_personajes)
+        elif opcion == '7':
+            menos_pesado(lista_personajes)
+        elif opcion == '8':
+            print("Saliendo del programa. ¡Hasta luego!")
+            break
+        else:
+            print("Opción no válida. Elija una opción del 1 al 8.")
+            input("Presione Enter para continuar...")
+else:
+    while True:
+        mostrar_segundo_menu()
+        opcion = input("Elija una opción (1-11, o 12 para salir): ")
+
+        if opcion == '1':
+            genero_m(lista_personajes)
+        elif opcion == '2':
+            genero_f(lista_personajes)
+        elif opcion == '3':
+            mas_alto_m(lista_personajes)
+        elif opcion == '4':
+            mas_alto_f(lista_personajes)
+        elif opcion == '5':
+            mas_bajo_m(lista_personajes)
+        elif opcion == '6':
+            mas_bajo_f(lista_personajes)
+        elif opcion == '7':
+            promedio_m(lista_personajes)
+        elif opcion == '8':
+            promedio_f(lista_personajes)
+        elif opcion == '9':
+            contador_color_ojos(lista_personajes)
+        elif opcion == '10':
+            contador_color_pelo(lista_personajes)
+        elif opcion == '11':
+            contador_inteligencia(lista_personajes)
+        elif opcion == '12':
+            print("Saliendo del programa. ¡Hasta luego!")
+            break
+        else:
+            print("Opción no válida. Elija una opción del 1 al 8.")
+            input("Presione Enter para continuar...")
